ae072d8a5b5c26b3d21bac696cab3ecef0b8b411
kielabokkie <kielabokkie@gmail.com>
2019-09-23 14:07:32 +1200

    Remove unused use statement

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,8 +2,6 @@
 
 namespace NotificationChannels\AwsPinpoint\Exceptions;
 
-use NotificationChannels\AwsPinpoint\Events\DeliveryFailed;
-
 class CouldNotSendNotification extends \Exception
 {
     public static function serviceRespondedWithAnError($exception)
